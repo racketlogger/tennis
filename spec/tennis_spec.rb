6b3da51a4e42051d291812a544317431e3936f01
Rohan Katyal <rohan12086@iiitd.ac.in>
2015-02-07 12:57:35 +0530

    bug fixes and added test cases

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -11,6 +11,57 @@ describe Tennis, "#scores" do
     score = Tennis.new("6-4, 6-4")
     expect(score.result).to eq 1
   end
+
+  it "finds the winner properly in three sets" do
+    score = Tennis.new("4-6, 6-2, 3-6")
+    expect(score.result).to eq 2
+  end
+
+  it "finds the winner properly in two sets with tie break" do
+    score = Tennis.new("6-4, 7-6")
+    expect(score.result).to eq 1
+  end
+
+  it "finds the winner properly in three sets with tie break" do
+    score = Tennis.new("6-4, 6-7, 7-6")
+    expect(score.result).to eq 1
+  end
+
+  it "reports incomplete match score (set 1-1)" do
+    score = Tennis.new("6-4,4-6")
+    expect(score.result).to eq :incomplete_match
+  end
+
+  it "reports incomplete match score (set incomplete)" do
+    score = Tennis.new("6-4,4-5")
+    expect(score.result).to eq :incomplete_match
+  end
+
+  it "checks invalid score: difference in games won < 2" do
+    score = Tennis.new("6-5,4-6,7-6")
+    expect(score.result).to eq :error
+  end
+
+  it "checks invalid score: only 1 set input" do
+    score = Tennis.new("6-4")
+    expect(score.result).to eq :error
+  end
+
+  it "checks invalid score: result decided in first 2 sets but 3rd set input" do
+    score = Tennis.new("6-4,6-4,4-6")
+    expect(score.result).to eq :error
+  end
+
+  it "checks invalid score: bad input for tie break" do
+    score = Tennis.new("7-0,4-6,6-2")
+    expect(score.result).to eq :error
+  end
+
+  it "checks invalid score: no score > 7" do
+    score = Tennis.new("8-4,2-6,6-1")
+    expect(score.result).to eq :error
+  end
+
 end
 
 describe Tennis, "#points" do
