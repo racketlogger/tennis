d5b195003537e8b40c612af686d732dd12e35292
Carlos Puchol <cpg+git@amahi.org>
2015-02-16 04:39:42 -0800

    cleanup tests about points, since we may not implement this in the future

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -63,47 +63,9 @@ describe Tennis, "#scores" do
 		score = Tennis.new("3-6,5-7")
 		expect(score.winner).to eq 1
 	end
-end
 
-describe Tennis, "#points" do
-	it "returns 12 points in a three set win in a complete match" do
-		pending "points being implemented"
-		score = Tennis.new("6-4, 4-6, 6-4")
-		expect(score.points).to eq [12,8]
-	end
-
-	it "returns 14 points in two set win in a complete match" do
-		pending "points being implemented"
-		score = Tennis.new("4-6, 4-6")
-		expect(score.points).to eq [8,14]
-	end
-
-	it "returns a max of 8 points for the runners up in a complete match" do
-		pending "points being implemented"
-		score = Tennis.new("4-6, 6-2, 3-6")
-		expect(score.points).to eq [8,12]
-	end
-
-	it "return a max of 10 points for each player in an incomplete match" do
-		pending "points being implemented"
-		score = Tennis.new("7-6,6-7")
-		expect(score.points).to eq [10,10]
-	end
-
-	it "return a max of 10 points for each player in an incomplete match" do
-		pending "points being implemented"
-		score = Tennis.new("7-6,4-6,4-1")
-		expect(score.points).to eq [10,10]
-	end
-
-	it "return [0,0] for bad input" do
+	it "raises exception for bad input" do
 		expect { Tennis.new("8-1") }.to raise_error
-	end
-
-	it "finds the points properly in scores with 7-5" do
-		pending "points being implemented"
-		score = Tennis.new("3-6,5-7")
-		expect(score.points).to eq [8,14]
 	end
 end
 
