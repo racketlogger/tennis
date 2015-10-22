369ff9f5176dd6d0a3a9c629daf282735a2ec06c
Carlos Puchol <cpg+git@amahi.org>
2015-10-21 19:49:01 -0700

    more cleanup

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,20 +1,22 @@
-require "tennis/version"
+require 'tennis/version'
 
+# Tennis class
+#
+# player 1 is the home player(0) , player 2 is the away player(1)
+# winner is either 0 or 1
+# points is an array: [points for 0 , points for 1]
+# sets_won/lost is an array: [sets won/lost by 0, sets won/lost by 1]
+# games_won/lost is an array: [games won/lost by 0, games won/lost by 1]
+#
+# representation of defaults:
+#
+# p0-<reason> means player 0 won, e.g. p0-win-by-forfeit
+# p1-<reason> means player 1 won
+#
+# the reason is ignored for the purposes of this gem.
+# possible reason fields are: win-by-forfeit, win-by-retired, win-by-no-show
+#
 class Tennis
-  # player 1 is the home player(0) , player 2 is the away player(1)
-  # winner is either 0 or 1
-  # points is an array: [points for 0 , points for 1]
-  # sets_won/lost is an array: [sets won/lost by 0, sets won/lost by 1]
-  # games_won/lost is an array: [games won/lost by 0, games won/lost by 1]
-
-  # representation of defaults:
-  #
-  # p0-<reason> means player 0 won, e.g. p0-win-by-forfeit
-  # p1-<reason> means player 1 won
-  #
-  # the reason is ignored for the purposes of this gem.
-  # possible reason fields are: win-by-forfeit, win-by-retired, win-by-no-show
-  #
 
   attr_reader :winner, :sets_won, :sets_lost, :games_won, :games_lost
 
@@ -32,13 +34,13 @@ class Tennis
   # to_s
   # return the score in string format
   def to_s
-    @default ? 'Default' : @score.map { | set| set.join('-') }.join(', ')
+    @default ? 'Default' : @score.map { |set| set.join('-') }.join(', ')
   end
 
   # flip score ( P1-P2 to P2-P1)
   # returns the flipped score as a string
   def flipped
-    @default ? 'Default' : @score.map { | set| set.reverse.join('-') }.join(', ')
+    @default ? 'Default' : @score.map { |set| set.reverse.join('-') }.join(', ')
   end
 
   def score
@@ -53,8 +55,10 @@ class Tennis
 
   def process_default(default)
     @score = default
-    @sets_won, @sets_lost = [[2, 0], [0, 2]]
-    @games_won, @games_lost = [[12, 0], [0, 12]]
+    @sets_won = [2, 0]
+    @sets_lost = [0, 2]
+    @games_won = [12, 0]
+    @games_lost = [0, 12]
     if default =~ /p0-win/
       # process as 6-0, 6-0
       @winner = 0
@@ -72,12 +76,12 @@ class Tennis
     begin
       sets = score.split(/,/)
       # only take 2 to 5 sets
-      fail "invalid number of sets" unless (2..best_of).cover? sets.length
+      fail 'invalid number of sets' unless (2..best_of).cover? sets.length
       score_plus_winner = map_scores_winners(sets)
       @set_winners = score_plus_winner.map { |sw| sw[1] }
       home = @set_winners.count(0)
       away = @set_winners.count(1)
-      fail "nobody won" if home + away == 0
+      fail 'nobody won' if home + away == 0
       @winner = home > away ? 0 : away > home ? 1 : fail('no winner')
       # sets won and lost
       @sets_won, @sets_lost = [[home, away], [away, home]]
@@ -85,8 +89,8 @@ class Tennis
       @score = score_plus_winner.map { |sw| sw[0] }
       @games_won = @score.transpose.map { |games| games.inject{ |sum, x| sum + x } }
       @games_lost = @games_won.reverse
-      # FIXME this is the only thing that assumes 3 sets
-      fail "too many sets" if @set_winners[0] == @set_winners[1] && sets.size > 2
+      # FIXME: this is the only thing that assumes 3 sets
+      fail 'too many sets' if @set_winners[0] == @set_winners[1] && sets.size > 2
     rescue => e
       fail ArgumentError, "invalid score '#{score}': #{e}"
     end
@@ -111,7 +115,7 @@ class Tennis
       return nil unless h > 5 || a > 5
       # either tie-break or else difference of one+
       return nil unless h + a == 13 || h > a + 1 || a > h + 1
-      return nil if h + a > 13 && (h-a).abs != 2
+      return nil if h + a > 13 && (h - a).abs != 2
       return h > a ? 0 : 1
     end
     # basic range check
