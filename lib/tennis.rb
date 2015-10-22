9e8bd1556c280a8cec953d552c90fb1c4648ee29
Carlos Puchol <cpg+git@amahi.org>
2015-10-21 19:40:36 -0700

    some code cleanup

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -32,17 +32,17 @@ class Tennis
   # to_s
   # return the score in string format
   def to_s
-    @default ? "Default" : @score.map{|set| set.join('-') }.join(', ')
+    @default ? 'Default' : @score.map { | set| set.join('-') }.join(', ')
   end
 
   # flip score ( P1-P2 to P2-P1)
   # returns the flipped score as a string
   def flipped
-    @default ? "Default" : @score.map{|set| set.reverse.join('-') }.join(', ')
+    @default ? 'Default' : @score.map { | set| set.reverse.join('-') }.join(', ')
   end
 
   def score
-    @default ? "Default" : @score
+    @default ? 'Default' : @score
   end
 
   def default?
@@ -64,7 +64,7 @@ class Tennis
       @sets_won, @sets_lost = [@sets_lost, @sets_won]
       @games_won, @games_lost = [@games_lost, @games_won]
     else
-      raise ArgumentError, "invalid default score report: '#{default}'"
+      fail ArgumentError, "invalid default score report: '#{default}'"
     end
   end
 
@@ -72,23 +72,23 @@ class Tennis
     begin
       sets = score.split(/,/)
       # only take 2 to 5 sets
-      raise ArgumentError, "invalid number of sets in '#{score}'" unless (2..best_of).cover? sets.length
+      fail "invalid number of sets" unless (2..best_of).cover? sets.length
       score_plus_winner = map_scores_winners(sets)
-      @set_winners = score_plus_winner.map{ |sw| sw[1] }
+      @set_winners = score_plus_winner.map { |sw| sw[1] }
       home = @set_winners.count(0)
       away = @set_winners.count(1)
-      raise ArgumentError, "nobody won in '#{score}'" if home + away == 0
-      @winner = home > away ? 0 : away > home ? 1 : raise("no winner")
+      fail "nobody won" if home + away == 0
+      @winner = home > away ? 0 : away > home ? 1 : fail('no winner')
       # sets won and lost
       @sets_won, @sets_lost = [[home, away], [away, home]]
       # score array
-      @score = score_plus_winner.map{ |sw| sw[0] }
-      @games_won = @score.transpose.map{ |games| games.inject{ |sum,x| sum + x } }
+      @score = score_plus_winner.map { |sw| sw[0] }
+      @games_won = @score.transpose.map { |games| games.inject{ |sum, x| sum + x } }
       @games_lost = @games_won.reverse
       # FIXME this is the only thing that assumes 3 sets
-      raise "too many sets in '#{score}'" if @set_winners[0] == @set_winners[1] and sets.size > 2
+      fail "too many sets" if @set_winners[0] == @set_winners[1] && sets.size > 2
     rescue => e
-      raise ArgumentError, "invalid score '#{score}': #{e}"
+      fail ArgumentError, "invalid score '#{score}': #{e}"
     end
   end
 
@@ -97,10 +97,10 @@ class Tennis
     sets.map do |set|
       set.strip!
       games = set.split(/-/).map(&:to_i)
-      raise "uneven games in set '#{set}'" unless games.length == 2
+      fail "uneven games in set '#{set}'" unless games.length == 2
       h, a = games
       sw = set_winner(h, a, set == sets.last)
-      raise ArgumentError, "no valid winner in set '#{set}'" unless sw
+      fail "no valid winner in set '#{set}'" unless sw
       [games, sw]
     end
   end
@@ -108,19 +108,19 @@ class Tennis
   # determine the set winner for home and away, or else return nil
   def set_winner(h, a, last_set = false)
     if last_set
-      return nil unless h > 5 or a > 5
+      return nil unless h > 5 || a > 5
       # either tie-break or else difference of one+
-      return nil unless h + a == 13 or h > a + 1 or a > h + 1
-      return nil if h + a > 13 and (h-a).abs != 2
+      return nil unless h + a == 13 || h > a + 1 || a > h + 1
+      return nil if h + a > 13 && (h-a).abs != 2
       return h > a ? 0 : 1
     end
     # basic range check
-    return nil if h > 7 or a > 7 or h < 0 or a < 0
+    return nil if h > 7 || a > 7 || h < 0 || a < 0
     # game went to 7
-    return 0 if h == 7 and [5,6].include?(a)
-    return 1 if a == 7 and [5,6].include?(h)
+    return 0 if h == 7 && [5, 6].include?(a)
+    return 1 if a == 7 && [5, 6].include?(h)
     # there has to be one winner, to 6
-    return nil unless (h == 6 and h > a + 1) or (a == 6 and a > h + 1)
+    return nil unless (h == 6 && h > a + 1) || (a == 6 && a > h + 1)
     h > a ? 0 : 1
   end
 
